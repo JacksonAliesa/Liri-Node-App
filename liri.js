014e20f6a344552cbd05b65faecde244da60f4c1
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-09 17:49:45 -0500

    added movie-this functions, movie populates with all required criteria

---
+++ liri.js
@@ -0,0 +1,92 @@
+require('dotenv').config();
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var axios = require('axios');
+
+// capture the command that the user puts in
+var action = process.argv[2]; //the action "concert-this, movie-this, "spotify", "do what it says""
+var userRequest = process.argv; //everything that the user types
+var userSearch = ''; // takes in everything starting at index of 3+-
+
+//for loop to go through every word in the node
+for (var i = 3; i < userRequest.length; i++) {
+	if (i > 3 && i < userRequest.length) {
+		userSearch = userSearch + '+' + userRequest[i];
+	} else {
+		userSearch += userRequest[i];
+	}
+}
+
+//check if usercommand is "concert-this"
+//run an API call using Axios to the bands-in-town API
+
+//switch statement
+
+switch (action) {
+	case 'concert-this':
+        console.log('command: ' + action);
+        console.log('Search: ' + userSearch);
+        
+		break;
+	case 'movie-this':
+        console.log('command: ' + action);
+        console.log('Search: ' + userSearch);
+        //need if else statement to tell user to insert valid movie title
+        // if (userRequest === userSearch){
+        //     userMovie();
+        // }
+        // else{
+        //     console.log('Insert Valid Movie Name')
+        // }
+        userMovie();
+    
+		break;
+	case 'spotfiy-this-song':
+        console.log('command: ' + action);
+        console.log('Search: ' + userSearch);
+		break;
+	case 'do-what-it-is':
+        console.log('command: ' + action);
+        console.log('Search: ' + userSearch);
+        break;
+        
+        default:
+            console.log("This is not a valid entry!")
+}
+
+
+function userMovie(){
+//inject the users search term in the queryURL
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = 'http://www.omdbapi.com/?t=' + userSearch + '&y=&plot=short&apikey=trilogy';
+console.log(queryUrl);
+
+// retrieves the requested data from OMDB
+axios.get(queryUrl).then(function(response) {
+    console.log('Title: ' + response.data.Title);
+    console.log('Release Year: ' + response.data.Year);
+    console.log('IMDB Rating: ' + response.data.imdbRating + "/10");
+    console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
+    console.log('Country: ' + response.data.Country);
+    console.log('Language: ' + response.data.Language);
+    console.log('Plot: ' + response.data.Plot);
+    console.log('Actors: ' + response.data.Actors);  
+});
+}
+
+//display the name of venue, venue location, and date of event
+
+//format the date of the event to be MM/DD/YYYY
+
+//check if userCommand
+
+//check if usercommand is "movie-this"
+
+//check if usercommand is "spotify-this-song"
+
+//check if usercommand is "do-what-it-is"
+
+//otherwise, display a message to the user that says try again
+
+//movie-this code
