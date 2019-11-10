59650a388d820da8bbfde3eaebbffb71bf2f6031
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-10 14:48:19 -0500

    add spotify-this section

--- liri.js
+++ liri.js
@@ -18,46 +18,68 @@ for (var i = 3; i < userRequest.length; i++) {
 	}
 }
 
-//check if usercommand is "concert-this"
-//run an API call using Axios to the bands-in-town API
-
 //switch statement
 
 switch (action) {
+	//case 1 to retrieve info from the BandsInTown API
 	case 'concert-this':
 		console.log('command: ' + action);
 		console.log('Search: ' + userSearch);
 		concertInfo();
 		break;
 
+	//case 2 to retrieve info from the OMDB API
 	case 'movie-this':
 		console.log('command: ' + action);
 		console.log('Search: ' + userSearch);
-		// need if else statement to tell user to insert valid movie title
-		// if (userRequest === userSearch){
-		//     userMovie();
-		// }
-		// else{
-		//     console.log('Insert Valid Movie Name')
+		// need if else statement just incase the user does not request a valid movie entry
+		//default to Mr. Nobody's movie info
+		// if (userSearch === "") {
+
+		// } else {
+		// 	console.log('Insert Valid Movie Name');
 		// }
 		userMovie();
 
 		break;
 
+	//case 3 to retrieve info from the Spotify API
 	case 'spotfiy-this-song':
 		console.log('command: ' + action);
 		console.log('Search: ' + userSearch);
+		// spotifyInfo();
 
+		// check if userCommand is "spotify-this-song"
+		// Using Spotify Node package info and documentation, make a call to the Spotify API using the user's search term
+
+		
+		spotify
+			.search({ type: 'artist OR album OR track', query: 'My Search Query', limit: 20 })
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+
+		// Display to the user:
+		// * Artist(s)
+		// * The song's name
+		// * A preview link of the song from Spotify
+		// * The album that the song is from
 		break;
 
+	//case 4 to retrieve info from the Spotify API when it is requested through a Txt file
 	case 'do-what-it-is':
 		console.log('command: ' + action);
 		console.log('Search: ' + userSearch);
 		break;
-
+	// when the criteria does not fit into any of the cases this is the default msg
 	default:
 		console.log('Try Again!');
 }
+
+//made functions for each action made by user. will call the function necessary for each case
 
 function userMovie() {
 	//inject the users search term in the queryURL
@@ -84,23 +106,19 @@ function concertInfo() {
 
 	// retrieves the requested data from bands in town API
 	axios.get(queryUrl).then(function(response) {
-		console.log(response.data);
-        //display the name of venue, venue location, and date of event
-        //format the date of the event to be MM/DD/YYYY
-        console.log(response.offers.venue.name);
-		
-		// console.log(response.data.datetime);
+		console.log('Upcoming concerts for ' + userSearch + ':');
+		//display the name of venue, venue location, and date of event
+		//format the date of the event to be MM/DD/YYYY
+		console.log(response.data[1].venue.name);
+		console.log(response.data[1].venue.city);
+		console.log(response.data[1].venue.region);
+		console.log(response.data[1].datetime);
 	});
 }
 
-//check if userCommand
-
-//check if usercommand is "movie-this"
-
-//check if usercommand is "spotify-this-song"
-
-//check if usercommand is "do-what-it-is"
-
-//otherwise, display a message to the user that says try again
-
-//movie-this code
+// function spotifyInfo() {
+// 	spotify.search({ type: userRequest, query: userSearch, limit: 20 }).then(function(response) {
+// 		console.log(response);
+// 		console.log(response.data);
+// 	});
+// }
