2797b49fc2b59dcdbee48857903c388e69502d7c
Aliesa Jackson <aliesaj2010@yahoo.com>
2019-11-11 20:12:23 -0500

    added the spotify-this-song function/set each function to a default setting if the user does not insert anything

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var axios = require('axios');
+var moment = require('moment');
+moment().format();
 
 // capture the command that the user puts in
 var action = process.argv[2]; //the action "concert-this, movie-this, "spotify", "do what it says""
@@ -23,9 +25,10 @@ for (var i = 3; i < userRequest.length; i++) {
 switch (action) {
 	//case 1 to retrieve info from the BandsInTown API
 	case 'concert-this':
-		console.log('command: ' + action);
-		console.log('Search: ' + userSearch);
-		concertInfo();
+		if (!userSearch) {
+			userSearch = 'u2';
+		}
+		concertInfo(userSearch);
 		break;
 
 	//case 2 to retrieve info from the OMDB API
@@ -34,43 +37,31 @@ switch (action) {
 		console.log('Search: ' + userSearch);
 		// need if else statement just incase the user does not request a valid movie entry
 		//default to Mr. Nobody's movie info
-		// if (userSearch === "") {
 
-		// } else {
-		// 	console.log('Insert Valid Movie Name');
-		// }
-		userMovie();
+		if (action === 'movie-this') {
+		    userMovie(userSearch);
+			}
+			
 
+		else if (!userSearch) {
+			userSearch = "Mr+Nobody"
+			userMovie(userSearch);
+		}
 		break;
 
 	//case 3 to retrieve info from the Spotify API
-	case 'spotfiy-this-song':
+	case 'spotify-this-song':
 		console.log('command: ' + action);
 		console.log('Search: ' + userSearch);
-		// spotifyInfo();
-
-		// check if userCommand is "spotify-this-song"
-		// Using Spotify Node package info and documentation, make a call to the Spotify API using the user's search term
-
-		
-		spotify
-			.search({ type: 'artist OR album OR track', query: 'My Search Query', limit: 20 })
-			.then(function(response) {
-				console.log(response);
-			})
-			.catch(function(err) {
-				console.log(err);
-			});
-
-		// Display to the user:
-		// * Artist(s)
-		// * The song's name
-		// * A preview link of the song from Spotify
-		// * The album that the song is from
+			// if no artist, track, or album is typed in default to Jhene Aiko
+		if (!userSearch) {
+			userSearch = 'Jhene Aiko';
+		}
+		spotifyInfo(userSearch);
 		break;
 
 	//case 4 to retrieve info from the Spotify API when it is requested through a Txt file
-	case 'do-what-it-is':
+	case 'do-what-it-says':
 		console.log('command: ' + action);
 		console.log('Search: ' + userSearch);
 		break;
@@ -81,7 +72,7 @@ switch (action) {
 
 //made functions for each action made by user. will call the function necessary for each case
 
-function userMovie() {
+function userMovie(userSearch) {
 	//inject the users search term in the queryURL
 	// Then run a request with axios to the OMDB API with the movie specified
 	var queryUrl = 'http://www.omdbapi.com/?t=' + userSearch + '&y=&plot=short&apikey=trilogy';
@@ -105,20 +96,74 @@ function concertInfo() {
 	console.log(queryUrl);
 
 	// retrieves the requested data from bands in town API
-	axios.get(queryUrl).then(function(response) {
+	axios.get(queryUrl).then(function(response, data) {
 		console.log('Upcoming concerts for ' + userSearch + ':');
+		console.log("-----------------------------------")
 		//display the name of venue, venue location, and date of event
-		//format the date of the event to be MM/DD/YYYY
-		console.log(response.data[1].venue.name);
-		console.log(response.data[1].venue.city);
-		console.log(response.data[1].venue.region);
-		console.log(response.data[1].datetime);
+
+
+		var bandSearch = response.data;
+		for(i = 0; i < bandSearch.length; i++){
+		console.log(bandSearch[i].venue.name);
+		console.log(bandSearch[i].venue.city);
+		console.log(bandSearch[i].venue.region);
+		//used to moment to format the date of event structure 
+		var concertDate = moment(response.data[i].datetime).format("MM/DD/YYYY hh:00 a")
+		console.log(concertDate);
+		console.log("-----------------------------------")
+		}
+		
+
+		
+
+
 	});
 }
 
-// function spotifyInfo() {
-// 	spotify.search({ type: userRequest, query: userSearch, limit: 20 }).then(function(response) {
-// 		console.log(response);
-// 		console.log(response.data);
-// 	});
-// }
+function spotifyInfo(userSearch) {
+	spotify.search({ type: 'track', query: userSearch, limit: 5 }, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+
+		// var spotifyArr = data.tracks.items;
+		var spotifyArr = data.tracks.items;
+		// to go loop through the object on spotify API
+		
+		for (i = 0; i < spotifyArr.length; i++) {
+			console.log("-----------------------------------")
+			//pulling artist
+			console.log("Artists: " + spotifyArr[i].artists[0].name);
+			//pull song title
+			console.log("Song Title: " + spotifyArr[i].name);
+			//pulling album
+			console.log("Album: " + spotifyArr[i].album.name);
+			//pull preview URL
+			console.log("Preview URL: " + spotifyArr[i].preview_url);
+			console.log("-----------------------------------")
+			
+			
+		}
+
+	
+	});
+}
+//end of function
+
+function dowhatitSays(userSearch){
+	// This block of code will read from the "random.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data"
+fs.readFile("random.txt", "utf8", function(error, data) {
+
+	// If the code experiences any errors it will log the error to the console.
+	if (error) {
+	  return console.log(error);
+	}
+  
+	// We will then print the contents of data
+	console.log(data);
+
+});
+}
+//end of function
